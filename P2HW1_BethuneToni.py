15172ce9c77435469a3f8725b122274df14dc9d9
beautyfullymade <bethunet7773@student.faytechcc.edu>
2025-03-22 21:57:29 -0400

    Add files via upload

--- P2HW1_BethuneToni.py
+++ P2HW1_BethuneToni.py
@@ -1,7 +1,9 @@
- # Toni Bethune
- # 03152025
- # P2HW1
- # create a program that does some basic math on numbers for a travel budget add dollar signs and have the amounts align.
+
+# Toni Bethune
+# 03152025
+# P2HW1
+# create a program that does some basic math on numbers for a travel budget add dollar signs and have the amounts align.
+
 
 
 
@@ -15,18 +17,19 @@
 budget = float(input("Enter Budget: "))
 destination = input("Enter your travel destination: ")
 fuel = float(input("How much do you think you will spend on gas? "))
-accommodation = float(input("Approximately, how much will you need for accommodation/hotel? "))
+hotel = float(input("Approximately, how much will you need for accommodation/hotel? "))
 food = float(input("Last, how much do you need for food? "))
 
-# Calculate remaining balance
-remaining_balance = budget - (fuel + accommodation + food)
 
-# Display formatted travel expenses
-print("\n------------Travel Expenses------------")
-print(f"{'Location:':<15} {destination}")
-print(f"{'Initial Budget:':<15} ${budget:,.2f}")
-print(f"{'Fuel:':<15} ${fuel:,.2f}")
-print(f"{'Accommodation:':<15} ${accommodation:,.2f}")
-print(f"{'Food:':<15} ${food:,.2f}")
-print("--------------------------------------")
-print(f"Remaining Balance: ${remaining_balance:,.2f}")
+total = fuel + hotel + food
+balance = budget - total
+
+
+print("\n------------ Travel Expenses ------------")
+print(f"{'Location:':15s}{destination}")
+print(f"{'Initial Budget:':15s}${budget:,.2f}")
+print(f"{'Fuel:':15s}${fuel:,.2f}")
+print(f"{'Accommodation:':15s}${hotel:,.2f}")
+print(f"{'Food:':15s}${food:,.2f}")
+print("-----------------------------------------")
+print(f"\n{'Remaining Balance:':15s}${balance:,.2f}")
